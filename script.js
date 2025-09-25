931d73a1369780263726d70ad41a43e92b855b40
aziza <azizaelhamyani40@gmail.com>
2025-09-25 08:59:45 +0100

    prototyp

---
+++ script.js
@@ -0,0 +1,15 @@
+// liste de competances
+let codes =["C1","C2","C3","C1","C2","C3","C1","C3"];
+//la competances a cherche
+let code ="C1";
+//compteur de la competances
+let count =0;
+// ror pour calculer le nomber de fois de C1
+for (let index = 0; index < codes.length; index++) {
+    if(codes[index]===code){
+
+       count++; 
+    }
+
+}  
+console.log(code+ " " +count);   
